826565f40b65c1b377d6460312adb03fb2aaf05e
Kaiwee <kaiweepuah0121@hotmail.com>
2018-02-12 22:24:19 +0800

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 	before_create { generate_token(:auth_token) }
 	attr_accessor :remember_me
 
-	enum role: { user: 0, admin: 1 }
+	enum role: { user: "0", admin: "1" }
 
 	validates :email, uniqueness: true, presence: true
 
