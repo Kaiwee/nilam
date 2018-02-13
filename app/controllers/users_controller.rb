9b6eacc416727fad936cd03909be666abe1622f6
Kaiwee <kaiweepuah0121@hotmail.com>
2018-02-13 13:43:52 +0800

    modify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_action :find_user, only: [:show, :edit, :update, :verify, :unverify]
+	before_action :find_user, only: [:show, :edit, :update]
 	before_action :check_current_user, only: [:edit, :update]
 
 	def create
